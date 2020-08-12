b513df185b5d62ae616502cb007150d3563d8379
Aravind Mantravadi <aravind1338@gmail.com>
2020-08-12 12:59:03 -0400

    Fixed sample in multiple studies issue

--- get_GEO_ids.py
+++ get_GEO_ids.py
@@ -1,7 +1,7 @@
 def main():
 	geo_ID_dict = {}
 	counter = 0
-	with open("test.txt") as file:
+	with open("gds_result.txt") as file:
 		for line in file:
 			counter += 1
 			if "Series		Accession:" in line:
@@ -18,16 +18,28 @@ def main():
 				terms = line.split(":")
 				geo_ID = terms[2]
 				geo_ID = geo_ID.rstrip("\n").strip()
+				
+				# Cases where the sample belongs to two different studies
+				geo_ID = geo_ID.split(" ")
+				if len(geo_ID) > 1:
+					# more than one study
+					for ID in geo_ID:
+						# Add the geo ID to the dictionary without repetition
+						if ID not in geo_ID_dict:
+							geo_ID_dict[geo_ID] = counter
 
-				# Add the geo ID to the dictionary without repetition
-				if geo_ID not in geo_ID_dict:
-					geo_ID_dict[geo_ID] = counter
+				else:
+					# Add the geo ID to the dictionary without repetition
+					if geo_ID[0] not in geo_ID_dict:
+						geo_ID_dict[geo_ID] = counter
 
 
-	"""for key in geo_ID_dict:
-		# key is the GEO ID, value happens to be the line number on which it was found
-		print (key, geo_ID_dict[key])"""
+			if counter % 10000 == 0:
+				print ("Finished %d lines" % counter)
 
+
+	# Write GEO ID's to text file
+	
 	with open ("geo_ids.txt", 'w') as f:
 		for key in geo_ID_dict:
 			f.write(key + "\n")
